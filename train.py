02730b94939538520a228fe665c2c23c2e66854f
j5950 <zaqwsx3255@naver.com>
2018-05-20 14:12:07 +0900

    Update train.py

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ from data_torchtext import *
 #=============================== Hyperparams ==========================================#
 hidden_dim = 1000
 mx_len = 50
-batch_size = 32
+batch_size = 16
 num_iter = 5
 embed_dim = 300
 dropout_p = 0.5
@@ -32,7 +32,7 @@ num_layer_gru = 2
 tensorboard_dir = './tensorboard'
 #=============================== Load data file =======================================#
 train_data, val_data, test_data, src_vocab, trg_vocab = get_data(
-    data_name='multi30k', batch_first=True)
+    data_name='WMT14DE', batch_first=True)
 train_iter = data.BucketIterator(
     dataset=train_data, repeat=False, shuffle=True, batch_size=batch_size, device=0, sort_within_batch=True, sort_key=lambda x: len(x.src))
 valid_iter = data.BucketIterator(
@@ -41,28 +41,17 @@ test_iter = data.BucketIterator(
     dataset=test_data, repeat=False, shuffle=True, batch_size=batch_size, device=0, sort_within_batch=True, sort_key=lambda x: len(x.src))
 
 
-'''for train_batch in train_iter:
-    print(train_batch.src[1][1])
-    print(train_batch.trg[1][1])
-    break'''
-
 
 
 def train(model, src_seqs, src_lens, trg_seqs, optimizer, criterion, is_valid):
     batch_size = src_seqs.size(0)
 
-# cuda, dev는 constant로 받기
 
     src_seqs_var = Variable(src_seqs.data.clone())
     trg_seqs_var = Variable(trg_seqs[:, :-1].data.clone()) # from <sos> to before <eos>
 
-    out = model(src_seqs_var, src_lens, trg_seqs_var) # L x V
-    '''    tmp = out.max(2)[1]
-    print("out size : {0}\n".format(out.size()))
-    print("model outcome")
-    print(tmp[0])
-    print("answer")
-    print(trg_seqs[0])'''
+    out = model(src_seqs_var, src_lens, trg_seqs_var) # B x L x V
+
     out = out.view(-1, len(trg_vocab)).contiguous() # B x L x V -> BL x V
 
     labels = trg_seqs[:, 1:].contiguous().view(-1) # except <sos>
@@ -80,38 +69,9 @@ def train(model, src_seqs, src_lens, trg_seqs, optimizer, criterion, is_valid):
 
     pred = out.max(1)[1]
     num_corrects = pred.data.eq(labels.data)
-    num_corrects = num_corrects.masked_select(labels.data.ne(trg_vocab.stoi['<pad>'])).sum() # pad idx == 0 ?
+    num_corrects = num_corrects.masked_select(labels.data.ne(trg_vocab.stoi['<pad>'])).sum()
     num_total = labels.data.ne(trg_vocab.stoi['<pad>']).sum()
     return loss, num_corrects, num_total
-
-
-def eval(model, src_seqs, src_lens, trg_seqs, trg_lens):
-    trg_seqs = trg_seqs[:, 1:]
-    src_seqs_var = Variable(src_seqs.data.clone())
-
-    out = model(src_seqs_var, src_lens)
-    labels = trg_seqs.clone() # B x L
-#    trg_lens_np = trg_lens.cpu().numpy()
-#    labels = labels[np.arange(0, batch_size), trg_lens_np]
-#    labels = trg_seqs.contiguous().view(-1)
-
-#    out = out.view(-1, trg_voca_size).contiguous() # B x L x V -> BL x V
-
-    pred = out.max(2)[1]
-#    print("@@")
-#    print(pred.size(-1))
-#    print(labels.size(-1))
-  #  num_corrects = pred.data.eq(labels.data)
-  #  num_corrects = num_corrects.masked_select(labels.data.ne(0)).sum() # pad idx == 0 ?
-
-    for i in range(batch_size):
-        print("answer")
-        print(labels[i].data)
-        print("length")
-        print(trg_lens[i])
-        print("predict")
-        print(pred[i])
-
 
 def save_results(model, src_seqs, src_lens,trg_seqs):
     print("save results sentence!")
@@ -163,8 +123,7 @@ for ep in range(num_epoch):
         src_seq = train_batch.src[0]
         src_len = train_batch.src[1]
         trg_seq = train_batch.trg[0]
-    #    print(src_len)
-    #    print(src_seq)
+
         loss, num_correct, num_total = train(seq2seq, src_seq, src_len.tolist(), trg_seq,
                                              optimizer, criterion, is_valid=False)
 
